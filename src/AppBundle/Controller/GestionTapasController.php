5b80ac42669701e48878c7b30e0e26d9812243df
espinosa <age.espinosa@gmail.com>
2019-03-24 11:41:51 -0300

    30 - Formularios. Clase AbstractType

--- src/AppBundle/Controller/GestionTapasController.php
+++ src/AppBundle/Controller/GestionTapasController.php
@@ -6,10 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Entity\Tapa;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use AppBundle\Form\TapaType;
 
 /**
  * @Route("/gestionTapas")
@@ -22,12 +19,7 @@ class GestionTapasController extends Controller
     public function nuevaTapaAction(Request $request)
     {
       $tapa = new Tapa();
-      $form= $this->createFormBuilder($tapa)
-                        ->add('nombre', TextType::class)
-                        ->add('descripcion', TextareaType::class)
-                        ->add('fechaCreacion', DateType::class)
-                        ->add('save', SubmitType::class, ['label' => 'Crear Tapa'])
-                        ->getForm();;
+      $form = $this->createForm(TapaType::class, $tapa);
       
     
       return $this->render('gestionTapas/nuevaTapa.html.twig', ['form' => $form->createView(),]);
