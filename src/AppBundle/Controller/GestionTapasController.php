bfeed86a7c1c0312356b4afe9395d8213b20fa61
espinosa <age.espinosa@gmail.com>
2019-03-24 11:22:44 -0300

    29 - Formularios. Campos

--- src/AppBundle/Controller/GestionTapasController.php
+++ src/AppBundle/Controller/GestionTapasController.php
@@ -8,6 +8,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Entity\Tapa;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 /**
  * @Route("/gestionTapas")
@@ -22,8 +24,9 @@ class GestionTapasController extends Controller
       $tapa = new Tapa();
       $form= $this->createFormBuilder($tapa)
                         ->add('nombre', TextType::class)
-                        ->add('descripcion', TextType::class)
-                        ->add('ingredientes', TextareaType::class)
+                        ->add('descripcion', TextareaType::class)
+                        ->add('fechaCreacion', DateType::class)
+                        ->add('save', SubmitType::class, ['label' => 'Crear Tapa'])
                         ->getForm();;
       
     
